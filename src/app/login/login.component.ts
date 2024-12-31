5017202865eb887c9f4f70af42e7fca00694bf95
Özkan Sarikaya <oezkan.sarikaya@freenet.de>
2024-12-31 10:24:32 +0100

    Add Login Page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../shared/footer/footer.component";
+import { HeaderComponent } from "../shared/header/header.component";
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, HeaderComponent, RouterLink, RouterOutlet],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
