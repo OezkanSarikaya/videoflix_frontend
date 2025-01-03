90817f916aa2736c4f8f6cb2cd3b7b938f9272cc
Özkan Sarikaya <oezkan.sarikaya@freenet.de>
2025-01-03 14:00:38 +0100

    Update Videoplayer with Overlay Icons on Hover

--- src/app/videoplayer/videoplayer.component.ts
+++ src/app/videoplayer/videoplayer.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-videoplayer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './videoplayer.component.html',
   styleUrl: './videoplayer.component.scss'
 })
 export class VideoplayerComponent {
+  @ViewChild('videoPlayer') videoPlayer?: ElementRef;
 
+  toggleVideo() {
+    const video: HTMLVideoElement = this.videoPlayer?.nativeElement;
+    video.paused ? video.play() : video.pause();
+  }
 }
